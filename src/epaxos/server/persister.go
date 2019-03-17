6b382c2a025ce76fd2e3bd14ea8022d008e76145
b1f6c1c4 <b1f6c1c4@gmail.com>
2019-03-16 20:07:24 -0400

    persister

--- src/epaxos/server/persister.go
+++ src/epaxos/server/persister.go
@@ -1,5 +1,6 @@
 package main
 
+import "io"
 import "github.com/lunixbochs/struc"
 import "epaxos/common"
 
@@ -19,6 +20,25 @@ func AppendLog(ep *common.EPaxos, rep common.ReplicaID, cmd *common.Command) err
 }
 
 func RecoverFromLog(ep *common.EPaxos) error {
-	// TODO
+	var err error
+	lst := ep.Array[ep.Self]
+	file := lst.LogFile
+	lst.Mu.Lock()
+	defer lst.Mu.Unlock()
+	if _, err = file.Seek(0, 0); err != nil {
+		return err
+	}
+	cmd := &common.Command{}
+	for {
+		if err = struc.Unpack(file, &cmd); err != nil {
+			if err == io.EOF {
+				break
+			}
+			return err
+		}
+		if cmd.Cmd == common.CmdPut {
+			ep.Data[cmd.Key] = cmd.Value
+		}
+	}
 	return nil
 }
