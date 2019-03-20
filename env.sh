d8ec2336e40e611d94dd3a5a53c797dae7e3667a
b1f6c1c4 <b1f6c1c4@gmail.com>
2019-03-20 14:44:34 -0400

    log tools

--- env.sh
+++ env.sh
@@ -3,3 +3,22 @@ export GOPATH="$PWD"
 go get github.com/lunixbochs/struc
 go get github.com/docopt/docopt-go
 go get golang.org/x/sync/semaphore
+
+rmdata() {
+	if [ "$1" != "-f" ]; then
+		echo -n '"sudo find ./data -type -f -delete" Are you sure? (y/N) '
+		read REPLY
+		if [ "$REPLY" != "y" ]; then
+			echo "Abort."
+			return
+		fi
+		echo
+	fi
+	sudo find ./data -type f -delete
+}
+export rmdata
+
+chkdata() {
+	find ./data -type f | xargs -r openssl sha1 | sort
+}
+export chkdata
